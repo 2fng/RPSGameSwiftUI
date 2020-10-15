5389e25d8daa41f11fa357e02ff5a4f5c4953b1a
Hua Son Tung <tunghuason@gmail.com>
2020-10-15 18:52:06 +0700

    Initial Commit

---
+++ rps game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rps game
+//
+//  Created by Hua Son Tung on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rps game/rps_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  rps_gameApp.swift
+//  rps game
+//
+//  Created by Hua Son Tung on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct rps_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
